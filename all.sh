ad9501c76b65bcf3a3729cea6906fc44e3340dac
Alex Shvartzkop <dudesuchamazing@gmail.com>
2024-07-28 08:26:53 +0300

    Build other libs.

--- all.sh
+++ all.sh
@@ -9,11 +9,15 @@ rm -rf ${CONF_SYSROOT}
 
 ./glu-build
 ./git-build # needs openssl
-./cairo-build
-./boost-build
+./cairo-build # builds freetype
+./boost-build # needs freetype
 ./ngspice-build
 ./occt-build # needs freetype
 ./odbc-build
+./zstd-build
+./harfbuzz-build  # needs freetype
+./protobuf-build
+./python-build
 ./wx-build
 ./kicad-build
 
